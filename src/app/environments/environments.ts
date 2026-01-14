6460d904cb18d692db743899492eff6215b603fe
cdwfs26-012 <cdwfs012@gmail.com>
2026-01-14 12:08:23 +0100

    Update environments.ts

--- src/app/environments/environments.ts
+++ src/app/environments/environments.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  API_BEARER_TOKEN:'reqres_f060f0bc93254230b40b4db1ad862106',
+  API_BEARER_TOKEN:'',
   STORAGE_KEY_CART:'auth_token_sushi_dev',
   API_LOGIN_URL:'https://reqres.in/api/login',
   AUTH_TOKEN_KEY:'auth_token_sushi_dev',
