c9e0f3bb62443822be0ab2253ffd8f25a8a351cb
CSI-WEB-4 <CSI-WEB-4@campus.local>
2026-01-14 12:13:45 +0100

    aptch

--- src/app/_pages/account/account.ts
+++ src/app/_pages/account/account.ts
@@ -1,12 +1,10 @@
 import { Component, inject } from '@angular/core';
 import { CommonModule, DecimalPipe, DatePipe } from '@angular/common'; // Importe DatePipe ici
-import { CartService } from '../../_services/cart';
 import {environment} from '../../environments/environments';
 
 @Component({
   selector: 'app-account',
   standalone: true,
-  // Ajoute DatePipe dans la liste des imports vvv
   imports: [CommonModule, DecimalPipe, DatePipe],
   templateUrl: './account.html'
 })
